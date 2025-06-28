5c06b75b8a57766502e96841709bb96e28c78d7a
Sonu Verma <115235554+sonu-verma1232@users.noreply.github.com>
2025-06-28 12:44:28 +0530

    second commit

---
+++ module1.mjs
@@ -0,0 +1,10 @@
+const a = "Harry";
+const b = "Rohan";
+const c = "Mohan";
+const d = "Sonu";
+
+export default d;
+
+export {a};
+export {b};
+export{c};
---
+++ module2.mjs
@@ -0,0 +1,6 @@
+import ui,{a,c,b} from "./module1.mjs";
+
+console.log(ui);
+console.log(a);
+console.log(b);
+console.log(c);
