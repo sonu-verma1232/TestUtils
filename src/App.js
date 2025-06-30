8b7ab2868ef1d07658fc1eb6ce9cc6b1916ce4d0
Sonu Verma <115235554+sonu-verma1232@users.noreply.github.com>
2025-06-30 09:42:27 +0530

    "Fifth Commit"

--- src/App.js
+++ src/App.js
@@ -3,14 +3,31 @@ import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import About from './components/About';
+import { useState } from 'react';
 
 function App() {
+
+  const[mode, setMode] = useState("light"); // it shows whether dark mode is enabled or not
+
+  const toggleMode = () =>  {
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor="grey";
+      document.body.style.color="white";
+    }
+    else{
+      setMode("light");
+      document.body.style.backgroundColor="white";
+      document.body.style.color="black";
+    }
+  } 
+
   return (
     <>
-      <Navbar title="TextUtils" aboutText="About TextUtils"></Navbar>
+      <Navbar title="TextUtils" aboutText="About TextUtils" mode={mode} toggleMode={toggleMode}></Navbar>
       {/* <Navbar></Navbar> */}
       <div className="container my-3">
-        <About/>
+        {/* <About/> */}
         <TextForm heading="Enter the text to analyze below"/>
       </div>
     </>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ import PropTypes from 'prop-types'
 export default function Navbar(props) {
     return (
 
-        <nav className="navbar navbar-dark bg-dark ">
+        <nav className={`navbar navbar-${props.mode} bg-${props.mode}`}>
             <div className="container-fluid">
                 <a className="navbar-brand" href="/">{props.title}</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -26,6 +26,10 @@ export default function Navbar(props) {
                         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
                         <button className="btn btn-outline-primary" type="submit">Search</button>
                     </form> */}
+                    <div className={`form-check form-switch text-${props.mode==='light' ? 'dark' : 'light'}`}>
+                        <input className="form-check-input" type="checkbox" role="switch" id="switchCheckDefault" onClick={props.toggleMode} />
+                        <label className="form-check-label" htmlFor="switchCheckDefault">Enable DarkMode</label>
+                    </div>
                 </div>
             </div>
         </nav>
